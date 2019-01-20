cdc05f64af5427fac624dbb18c11608e13f48227
Isaiah Magett <imagett10@gmail.com>
2019-01-20 02:12:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, location)
   index = board[location.to_i - 1]
-  index != " " && index != "" && index != nil && index == "O" && index == "X"
+  index != " " && index != "" && index != nil && index != "O" && index != "X"
   
 end
